ea187fbc68b2ed042346ee306a4ba4efe5ff9f1e
Shivam <shivampanigrahi38@gmail.com>
2025-07-01 13:15:51 +0530

    second commit

---
+++ mavendemo/src/test/java/bikes/Bajaj.java
@@ -0,0 +1,20 @@
+package bikes;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Bajaj {
+	
+	@Test
+	public void openHomepage() throws InterruptedException {
+		WebDriver driver=new FirefoxDriver();
+		driver.get("https://www.bajajauto.com/");
+		Reporter.log("Bajaj class is executed",true);
+		Thread.sleep(1000);
+		driver.close();
+	}
+	
+
+}
