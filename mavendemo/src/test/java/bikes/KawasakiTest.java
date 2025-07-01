23697e4b5faf7a7fcea5c8d5933186ea28ed4d5a
shivampanigrahi214 <shivam.panigrahi98@gmail.com>
2025-07-01 13:06:14 +0530

    Update KawasakiTest.java

--- mavendemo/src/test/java/bikes/KawasakiTest.java
+++ mavendemo/src/test/java/bikes/KawasakiTest.java
@@ -14,12 +14,6 @@ public class KawasakiTest {
 		Thread.sleep(1000);
 		driver.close();
 	}
-	public void openHomepage() throws InterruptedException {
-		WebDriver driver=new FirefoxDriver();
-		driver.get("https://www.kawasaki-india.com/");
-		Reporter.log("KawasakiTest class is executed",true);
-		Thread.sleep(1000);
-		driver.close();
-	}
+	
 
 }
